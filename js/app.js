9b166535ae67c00d72f9bc507cd53c48edbad0b8
Bruno Victor Oliveira Duarte <duartebruno581@gmail.com>
2022-08-30 17:59:06 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 function getUserPosition() {
     let url;
-    navigator.geolocation.getCurrentPosition((pos) => {
-      let lat = pos.coords.latitude;
-      let long = pos.coords.longitude;
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
       url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=95b11822eb429c84c1143a19251b1881`;
       fetchApi(url);
     });
@@ -30,20 +30,22 @@ function getUserPosition() {
       let humidityPorce = (data.main.humidity);
       let cloudsPorce = (data.clouds.all);
       let WindDesc = (1.60934421012 * data.wind.speed).toFixed(1);
-      city.innerText = `${data.name} - ${data.sys.country}`;
+      city.innerText = ` ${data.name} - ${data.sys.country}`;
       temp.innerText = tempInCelsius;
       humidity.innerText = humidityPorce + ' %';
       wind.innerText = WindDesc + ' Km/h';
       clouds.innerText = cloudsPorce + ' %';
-      temp_max.innerText = tempInCelsiusMax + 'ºC';
-      temp_min.innerText = tempInCelsiusMin + 'ºC';
-        if (humidityPorce == 0){
-            chuva.innerText = "Baixas previsões de chuva";
+      temp_max.innerText = tempInCelsiusMax + 'º';
+      temp_min.innerText = tempInCelsiusMin + 'º';
+        if (humidityPorce < 30){
+            chuva.innerText = "Umidade baixa";
+            document.getElementById('chuva').style.color = 'red';
+            document.getElementById('avisoChuva').onclick = Alerta;
         }
-        if (humidityPorce < 50 ) {
-            chuva.innerText = "Chuvas médias na região";
+        if (humidityPorce < 70) {
+            chuva.innerText = "Previsões baixas de chuva";
         }
-        else{
+        else {
             chuva.innerText = "Pancadinhas de chuva";
         }
 
@@ -54,17 +56,40 @@ function getUserPosition() {
             vento.innerText = "Ventos Médios";
         }
         else{
+            document.getElementById('vento').style.color = 'red';
+            document.getElementById('avisoVento').onclick = Alerta;
             vento.innerText = "Rajadas de vento";
         }
 
         if (cloudsPorce < 20){
             ceu.innerText = "Céu limpo";
+            document.getElementById('wrapper').style.backgroundColor = '#82b9dc';
+            var img = document.createElement("IMG");
+            img.src = "/img/sunny.png";
+            img.style.width = "140px";
+            img.style.height = "140px";
+            document.getElementById('img').appendChild(img);
         }
+
         if (cloudsPorce < 50 ) {
-            ceu.innerText = "Céu nublado";
+            ceu.innerText = "Céu parcialmente nublado";
+            document.getElementById('wrapper').style.backgroundColor = '#009abc';
+            var img = document.createElement("IMG");
+            img.src = "/img/cloud.png";
+            img.style.width = "140px";
+            img.style.height = "140px";
+            document.getElementById('img').appendChild(img);
         }
         else{
             ceu.innerText = "Céu muito nublado";
+            document.getElementById('wrapper').style.backgroundColor = '#027b96';
+            var img = document.createElement("IMG");
+            img.src = "/img/rain.png";
+            img.style.width = "140px";
+            img.style.height = "140px";
+            document.getElementById('img').appendChild(img);
+            document.getElementById('ceu').style.color = 'red';
+            document.getElementById('avisoCeu').onclick = Alerta;
         }
     })
     .catch((err) => {
@@ -75,6 +100,28 @@ function getUserPosition() {
   
   getUserPosition();
 
+  function Alerta(){
+    window.location.assign("https://portal.inmet.gov.br/");
+  }
+  
   function Reload (){
     location.reload();
   }
+
+  function notifyMe() {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      const notification = new Notification("Agora você receberá nossas notificações! ;)");
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          const notification = new Notification("Agora você receberá nossas notificações! ;)");
+        }
+      });
+    }
+  }
+
+  let time = document.getElementById('time');
+  const date = new Date().toLocaleString();
+  time.innerText = date;
